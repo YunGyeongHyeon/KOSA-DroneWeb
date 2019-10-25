68629318b23c1fedbd1deeb496e4cc95e27a5646
user12 <user12@iot.com>
2019-10-25 20:00:38 +0900

    title: front-view design
    content: main.jsp & map.jsp view design

--- src/main/java/com/fireBusters/web/controller/HomeController.java
+++ src/main/java/com/fireBusters/web/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.fireBusters.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("/user")
 public class HomeController {
 	@RequestMapping("/")
 	public String main() {
