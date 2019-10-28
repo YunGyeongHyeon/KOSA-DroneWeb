f131f5729cdf401d1646467500b668514370dae7
user20 <user20@iot.com>
2019-10-28 13:43:59 +0900

    위도 경도 값 insert

--- src/main/java/com/fireBusters/web/dao/UserDao.java
+++ src/main/java/com/fireBusters/web/dao/UserDao.java
@@ -1,5 +1,8 @@
 package com.fireBusters.web.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,8 +14,11 @@ public class UserDao {
 	private SqlSessionTemplate sqlSessionTemplate;
 
 	public int insert(double lat, double lon) {
-		int row = sqlSessionTemplate.insert("");
-		
+		Map<String, Double> map = new HashMap<>();
+		map.put("lat", lat);
+		map.put("lon", lon);
+		int rows = sqlSessionTemplate.insert("user.insert", map);
+		return rows;
 	}
 
 }
