e04218b39a59af5f7a09d80a913e63e38668cf8c
user19 <user19@iot.com>
2019-10-28 12:54:57 +0900

    위도 경도 수정

--- src/main/java/com/fireBusters/web/controller/HomeController.java
+++ src/main/java/com/fireBusters/web/controller/HomeController.java
@@ -1,10 +1,19 @@
 package com.fireBusters.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fireBusters.web.dto.UserMember;
+import com.fireBusters.web.service.UserService;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService service;
+	
+	
 	@RequestMapping("/")
 	public String main() {
 		return "/user/main";
@@ -17,7 +26,9 @@ public class HomeController {
 	}
 
 	@RequestMapping("/complete")
-	public String complete() {
+	public String complete(double lat, double lng) {
+		service.writeLocation(lat, lng);
 		return "/user/complete";
 	}
+
 }
---
+++ src/main/java/com/fireBusters/web/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.fireBusters.web.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.fireBusters.web.dto.UserMember;
+
+@Controller
+public class UserDao {
+	
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	public void insert(double lat, double lng) {
+		
+	}
+
+}
